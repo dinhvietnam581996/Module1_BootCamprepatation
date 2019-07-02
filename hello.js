d9bf6ac58aa4ea44af85b76f46cf70c22dbe77b7
dinhvietnam <dinhvietnam581998>
2019-07-02 14:15:19 +0700

    update

---
+++ hello.js
@@ -0,0 +1,2 @@
+alert("xin chào các bạn");
+document.write("<h1>Hello các bạn<h1>");
---
+++ java.js
@@ -0,0 +1,4 @@
+document.getElementById("demo1").innerHTML = "diện tích hình chữ nhật";
+document.getElementById("demo2").innerHTML = "diện tích hình vuông";
+document.getElementById("demo3").innerHTML = "diện tích hình tròn";
+document.getElementById("demo4").innerHTML = "diện tích hình tam giác";
